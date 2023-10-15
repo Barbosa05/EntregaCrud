fde69ec775c231936137898ff26dc21217eb9483
ferna <ferna@Fernandao>
2023-10-15 11:01:36 -0300

    CrudEntrega

---
+++ crud/src/crud/Empresa.java
@@ -0,0 +1,32 @@
+package crud;
+
+import java.util.ArrayList;
+
+public class Empresa implements Gerenciamento {
+   private ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+   @Override
+   public void addPessoa(Pessoa pessoa) {
+       pessoas.add(pessoa);
+   }
+
+   @Override
+   public Pessoa getPessoa(String nome){
+       for (Pessoa pessoa: pessoas) {
+           if(pessoa.getNome().equals(nome))
+               return pessoa;
+       }
+
+       return null;
+   }
+   @Override
+   public void updatePessoa(String nome, Pessoa pessoa) {
+       var index = pessoas.indexOf(pessoa);
+       pessoas.set(index, pessoa);
+   }
+
+    @Override
+    public void deletePessoa(String nome) {
+        pessoas.removeIf(pessoa -> pessoa.getNome().equals(nome));
+    }
+}
---
+++ crud/src/crud/Gerenciamento.java
@@ -0,0 +1,8 @@
+package crud;
+
+public interface Gerenciamento {
+    void addPessoa(Pessoa pessoa);
+    Pessoa getPessoa(String nome);
+    void updatePessoa(String nome, Pessoa pessoa);
+    void deletePessoa(String nome);
+}
